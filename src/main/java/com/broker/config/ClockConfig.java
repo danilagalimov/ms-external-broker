8d0d729106ef478b5f529f792fd06dde4aac99f9
Danila Galimov <danilagalimov@gmail.com>
2023-01-08 21:39:00 +0200

    Add missing @EnableAsync annotation

--- src/main/java/com/broker/config/ClockConfig.java
+++ src/main/java/com/broker/config/ClockConfig.java
@@ -2,11 +2,14 @@ package com.broker.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 import java.time.Clock;
 
 @Configuration
+@EnableAsync
 public class ClockConfig {
+
     @Bean
     public Clock clock() {
         return Clock.systemDefaultZone();
